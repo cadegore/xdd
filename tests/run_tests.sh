931821f4bdf47e78b8beba52d302066717ed6b10
MigeljanImeri <imerimigel@gmail.com>
2023-05-04 11:13:18 -0400

    Updated workflow and configure.ac
    Hopefully fixed some pathing errors with running the tests, along with updating
    the configure.ac to use non-obsolete syntax.
    
    Signed-off-by: MigeljanImeri <imerimigel@gmail.com>

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,11 +2,11 @@
 
 #Setup functional tests 
 XDD_EXE_PATH=$(which xdd)
-echo XDDTEST_XDD_EXE=$XDD_EXE_PATH > tests/test_config
-echo XDDTEST_TESTS_DIR=. >> tests/test_config
-echo XDDTEST_LOCAL_MOUNT=$HOME/test-dir >> tests/test_config
+echo XDDTEST_XDD_EXE=$XDD_EXE_PATH > ./test_config
+echo XDDTEST_TESTS_DIR=. >> ./test_config
+echo XDDTEST_LOCAL_MOUNT=$HOME/test-dir >> ./test_config
 
-cp tests/test_config tests/functional/test_config
+mv ./test_config ./tests/functional/test_config
 
 #Run functional tests
 for f in tests/functional/*.sh; do
