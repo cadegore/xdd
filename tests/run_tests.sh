01b1e9f775e17c3633bfdbdf9cbd99c7d3f6325d
MigeljanImeri <imerimigel@gmail.com>
2023-05-04 12:06:35 -0400

    Iteration on workflow procedure
    Hopefully fixed pathing issues with test script.
    
    Signed-off-by: MigeljanImeri <imerimigel@gmail.com>

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,9 +6,12 @@ echo XDDTEST_XDD_EXE=$XDD_EXE_PATH > ./test_config
 echo XDDTEST_TESTS_DIR=. >> ./test_config
 echo XDDTEST_LOCAL_MOUNT=$HOME/test-dir >> ./test_config
 
-mv ./test_config ./tests/functional/test_config
+# mv ./test_config ./tests/functional/test_config
 
 #Run functional tests
+set -e
+status=0
 for f in tests/functional/*.sh; do
-  bash "$f" || break
+  bash "$f"
+  status=$(($status + $?)) 
 done
